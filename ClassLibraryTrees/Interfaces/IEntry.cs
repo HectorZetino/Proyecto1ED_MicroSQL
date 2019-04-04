58d08c856070c066435e891d12503bd67c5c36e4
Hector <h.rzc.zetino@gmail.com>
2019-04-03 22:09:58 -0600

    Creacion Repositorio

---
+++ ClassLibraryTrees/Interfaces/IEntry.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibraryTrees.B_Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTrees.Interfaces
+{
+    public interface IEntry<TKey, T>
+    {
+        bool Equals(Entry<TKey, T> other);
+    }
+}
